8efb94831eecdc548bba0a40243bc6cf95031dd5
holysabre <1026579245@qq.com>
2023-04-12 11:20:13 +0800

    feat:从实际运行的目录获取路径

--- utils/os_util.go
+++ utils/os_util.go
@@ -21,13 +21,17 @@ func GetOsType() string {
 }
 
 func GetBinDir() string {
-	wd, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-	}
-	absPath, err := filepath.Abs(wd)
+	// 获取当前可执行文件的绝对路径
+	exePath, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
-	return absPath + "/bin"
+
+	// 获取可执行文件所在的目录路径
+	exeDir := filepath.Dir(exePath)
+
+	// 将目录路径拼接上您需要的文件夹名字，例如"templates"
+	templatesDir := filepath.Join(exeDir, "bin")
+	fmt.Println("templatesDir:", templatesDir)
+	return templatesDir
 }
